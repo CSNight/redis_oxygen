8e000f0ababf0572d3a3391ae6634d3b4d9d2337
S CH <923739637@qq.com>
2019-11-19 23:08:58 +0800

    user add/edit function complete

--- src/api/system/user_api.js
+++ src/api/system/user_api.js
@@ -3,7 +3,11 @@ import REQ from '../../api/services'
 export function get_users() {
     return REQ.get('/users/get_users');
 }
-
+export function query_users(cond) {
+    return REQ.get('/users/query_users', {
+        params: cond
+    });
+}
 export function get_org_user(id) {
     return REQ.get('/users/get_by_org/' + id)
 }
@@ -14,4 +18,8 @@ export function new_user(user_ins) {
 
 export function edit_user(user_ins) {
     return REQ.put('/users/edit_user', user_ins);
+}
+
+export function delete_user(id) {
+    return REQ.delete('/users/delete_by_id/' + id);
 }
