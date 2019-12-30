4197970c9f30f5f23481af5e0af70eeedc18f2da
chensi <chensi@thtf.com.cn>
2019-12-30 17:28:41 +0800

    增加sql监控菜单

---
+++ src/views/system/database/DbMonitor.vue
@@ -0,0 +1,21 @@
+<template>
+    <i-frame :src="src"/>
+</template>
+
+<script>
+    import IFrame from "../../../components/IFrame";
+
+    export default {
+        name: "DbMonitor",
+        components: {IFrame},
+        data() {
+            return {
+                src: this.$store.getters.baseUrl + "/druid/index.html"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
