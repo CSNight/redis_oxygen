d931dca9769d23584504dec47f32320f4767e7ef
S CH <923739637@qq.com>
2020-01-08 15:11:46 +0800

    add refresh key and expire key request

--- src/api/redismanage/redis_keys.js
+++ src/api/redismanage/redis_keys.js
@@ -11,8 +11,19 @@ export function getKeyValue(dto) {
         params: dto
     });
 }
-export function deleteKey(dto) {
+
+export function refreshKey(dto) {
+    return REQ.get('/keys/refresh', {
+        params: dto
+    });
+}
+
+export function deleteKeys(dto) {
     return REQ.delete('/keys/delete', {
         data: dto
     });
+}
+
+export function expireKeys(dto, type) {
+    return REQ.put('/keys/expires/' + type, dto);
 }
