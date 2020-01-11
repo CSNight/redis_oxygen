c9e8de7e0d1da4dea780a772a8a23b9c23faab8b
S CH <923739637@qq.com>
2020-01-11 17:04:20 +0800

    add ace code editor

---
+++ src/components/AceEditor.vue
@@ -0,0 +1,106 @@
+<script>
+    // eslint-disable-next-line no-undef
+    var ace = require('brace');
+    export default {
+        name: "AceEditor",
+        render: function (h) {
+            let height = this.height ? this.px(this.height) : '100%';
+            let width = this.width ? this.px(this.width) : '100%';
+            return h('div', {
+                attrs: {
+                    style: "height: " + height + '; width: ' + width,
+                }
+            })
+        },
+        props: {
+            value: String,
+            lang: {
+                type: String,
+                required: true
+            },
+            theme: String,
+            height: {
+                type: Number,
+            },
+            width: {
+                type: Number,
+            },
+            options: {
+                type: Object,
+            },
+        },
+        data: function () {
+            return {
+                editor: null,
+                contentBackup: ""
+            }
+        },
+        methods: {
+            px: function (n) {
+                if (/^\d*$/.test(n)) {
+                    return n + "px";
+                }
+                return n;
+            }
+        },
+        watch: {
+            value: function (val) {
+                if (this.contentBackup !== val) {
+                    this.editor.session.setValue(val, 1);
+                    this.contentBackup = val;
+                }
+            },
+            theme: function (newTheme) {
+                this.editor.setTheme('ace/theme/' + newTheme);
+            },
+            lang: function (newLang) {
+                this.editor.getSession().setMode(typeof newLang === 'string' ? ('ace/mode/' + newLang) : newLang);
+            },
+            options: function (newOption) {
+                this.editor.setOptions(newOption);
+            },
+            height: function () {
+                this.$nextTick(function () {
+                    this.editor.resize()
+                })
+            },
+            width: function () {
+                this.$nextTick(function () {
+                    this.editor.resize()
+                })
+            }
+        },
+        beforeDestroy: function () {
+            this.editor.destroy();
+            this.editor.container.remove();
+        },
+        mounted: function () {
+            var vm = this;
+            var lang = this.lang || 'text';
+            var theme = this.theme || 'monokai';
+
+            // eslint-disable-next-line no-undef
+            require('brace/ext/emmet');
+
+            var editor = vm.editor = ace.edit(this.$el);
+            editor.$blockScrolling = Infinity;
+
+            this.$emit('init', editor);
+
+            //editor.setOption("enableEmmet", true);
+            editor.getSession().setMode(typeof lang === 'string' ? ('ace/mode/' + lang) : lang);
+            editor.setTheme('ace/theme/' + theme);
+            if (this.value)
+                editor.setValue(this.value, 1);
+            this.contentBackup = this.value;
+
+            editor.on('change', function () {
+                var content = editor.getValue();
+                vm.$emit('input', content);
+                vm.contentBackup = content;
+            });
+            if (vm.options)
+                editor.setOptions(vm.options);
+        }
+    }
+</script>
---
+++ src/views/toolbox/code/CodeEditor.vue
@@ -0,0 +1,186 @@
+<template>
+    <el-card style="margin-top: 20px">
+        <div slot="header">
+            <span>代码编辑</span>
+            <div class="card-op">
+                <span style="margin-right: 5px">Themes:</span>
+                <el-select size="mini" v-model="theme">
+                    <el-option v-for="item in themes" :key="item.theme" :label="item.caption" :value="item.name"/>
+                </el-select>
+            </div>
+            <div class="card-op">
+                <span style="margin-right: 5px">Languages:</span>
+                <el-select size="mini" type="text" v-model="language" @change="langChange">
+                    <el-option v-for="item in languages" :key="item.name" :label="item.caption" :value="item.name"/>
+                </el-select>
+            </div>
+            <div class="card-op">
+                <el-button v-if="language==='html'" type="primary" size="mini" @click="submit">运行</el-button>
+            </div>
+        </div>
+        <div style="width:100%;height: 75vh;" :class="language==='html'?'preview-code':''">
+            <ace-editor ref="editor" v-model="code" @init="editorInit" :options="options" :theme="theme"
+                        :lang="language"/>
+        </div>
+        <div style="width: 50%;height: 76vh;float: right" v-if="language==='html'">
+            <iframe style="top: 0;background: #fff;width: 100%;height: 100%" class="edit-area" id="preview"
+                    frameborder="0"></iframe>
+        </div>
+    </el-card>
+</template>
+
+<script>
+    import AceEditor from "../../../components/AceEditor";
+
+
+    export default {
+        name: "CodeEditor",
+        components: {AceEditor},
+        created() {
+            this.$nextTick(() => {
+
+            })
+        },
+        data() {
+            return {
+                code: '',
+                codeJava: 'package com.text.code;\n' +
+                    '\n' +
+                    '/**\n' +
+                    ' * @author csnight\n' +
+                    ' * @description\n' +
+                    ' * @since 2019/12/31 13:50\n' +
+                    ' */\n' +
+                    'public class Test {\n' +
+                    '    public static void main(String[] args) {\n' +
+                    '        System.out.println("Hello World!");\n' +
+                    '    }\n' +
+                    '}\n',
+                codeHtml: '<html lang="en">\n' +
+                    '    <body>\n' +
+                    '        HelloWorld\n' +
+                    '    </body>\n' +
+                    '</html>',
+                theme: 'monokai', language: 'java',
+                options: {
+                    wrap: true,
+                    highlightActiveLine: true,
+                    autoScrollEditorIntoView: true,
+                    enableBasicAutocompletion: true,
+                    enableSnippets: true,
+                    showPrintMargin: false,
+                    enableLiveAutocompletion: true
+                },
+                themeData: [
+                    ['Chrome'],
+                    ['Clouds'],
+                    ['Crimson Editor'],
+                    ['Dawn'],
+                    ['Dreamweaver'],
+                    ['Eclipse'],
+                    ['GitHub'],
+                    ['IPlastic'],
+                    ['Solarized Light'],
+                    ['TextMate'],
+                    ['Tomorrow'],
+                    ['XCode'],
+                    ['Kuroir'],
+                    ['KatzenMilch'],
+                    ['SQL Server', 'sqlserver', 'light'],
+                    ['Ambiance', 'ambiance', 'dark'],
+                    ['Chaos', 'chaos', 'dark'],
+                    ['Clouds Midnight', 'clouds_midnight', 'dark'],
+                    ['Dracula', '', 'dark'],
+                    ['Cobalt', 'cobalt', 'dark'],
+                    ['Gruvbox', 'gruvbox', 'dark'],
+                    ['Green on Black', 'gob', 'dark'],
+                    ['idle Fingers', 'idle_fingers', 'dark'],
+                    ['krTheme', 'kr_theme', 'dark'],
+                    ['Merbivore', 'merbivore', 'dark'],
+                    ['Merbivore Soft', 'merbivore_soft', 'dark'],
+                    ['Mono Industrial', 'mono_industrial', 'dark'],
+                    ['Monokai', 'monokai', 'dark'],
+                    ['Pastel on dark', 'pastel_on_dark', 'dark'],
+                    ['Solarized Dark', 'solarized_dark', 'dark'],
+                    ['Terminal', 'terminal', 'dark'],
+                    ['Tomorrow Night', 'tomorrow_night', 'dark'],
+                    ['Tomorrow Night Blue', 'tomorrow_night_blue', 'dark'],
+                    ['Tomorrow Night Bright', 'tomorrow_night_bright', 'dark'],
+                    ['Tomorrow Night 80s', 'tomorrow_night_eighties', 'dark'],
+                    ['Twilight', 'twilight', 'dark'],
+                    ['Vibrant Ink', 'vibrant_ink', 'dark']
+                ],
+                languages: [
+                    {name: 'java', caption: 'Java'},
+                    {name: 'csharp', caption: 'C#'},
+                    {name: 'python', caption: 'Python'},
+                    {name: 'html', caption: 'Html'},
+                    {name: 'javascript', caption: 'Javascript'},
+                    {name: 'json', caption: 'Json'},
+                    {name: 'sql', caption: 'SQL'},
+                    {name: 'xml', caption: 'XML'},
+                    {name: 'yaml', caption: 'YAML'}],
+
+                themes: []
+            }
+        },
+        methods: {
+            editorInit: function () {
+                // eslint-disable-next-line no-undef
+                require('brace/ext/language_tools');//language extension prerequsite...
+                for (let i = 0; i < this.languages.length; i++) {
+                    // eslint-disable-next-line no-undef
+                    require('brace/mode/' + this.languages[i].name);
+                    // eslint-disable-next-line no-undef
+                    require('brace/snippets/' + this.languages[i].name);
+                }
+                // eslint-disable-next-line no-undef
+                require('brace/theme/monokai'); //snippet
+                this.getThemes();
+                this.code=this.codeJava;
+            }, getThemes() {
+                this.themes = this.themeData.map(function (data) {
+                    let name = data[1] || data[0].replace(/ /g, '_').toLowerCase();
+                    // eslint-disable-next-line no-undef
+                    require('brace/theme/' + name); //snippet
+                    return {
+                        caption: data[0],
+                        theme: 'brace/theme/' + name,
+                        isDark: data[2] === 'dark',
+                        name: name
+                    };
+                });
+            }, submit() {
+                let htmlValue = this.$refs.editor.editor.getValue();
+                document.getElementById('preview').srcdoc = '<!DOCTYPE html><html lang=""><head><meta charset="utf-8" /><title>代码测试</title>' +
+                    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>' +
+                    '<meta name="format-detection" content="telephone=no" />' +
+                    '<meta name="apple-mobile-web-app-status-bar-style" content="black" />' +
+                    '<meta name="apple-mobile-web-app-capable" content="yes" />' +
+                    '<meta http-equiv="X-UA-Compatible" content="chrome=1,IE=edge"/>' +
+                    '</head><body>' + htmlValue + '</body></html>';
+            }, langChange() {
+                if (this.language === 'html') {
+                    this.code = this.codeHtml;
+                } else {
+                    this.code = this.codeJava;
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .card-op {
+        float: right;
+        display: flex;
+        align-items: center;
+        margin-right: 10px;
+        font-size: 12px
+    }
+
+    .preview-code {
+        width: 50% !important;
+        float: left;
+    }
+</style>
