b8bf355a6267149a804e61670c929f999d19909b
S CH <923739637@qq.com>
2020-01-12 19:03:34 +0800

    add markdown tool

---
+++ src/views/toolbox/markdown/MarkDownTool.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-card style="height: 86vh;width: 100%;margin-top: 20px;">
+        <div slot="header">Markdown - support by <a style="text-decoration: underline;color:#2bf" href="https://github.com/hinesboy/mavonEditor">MavonEditor</a></div>
+        <mavonEditor style=" height: 80vh; width: 100%;"/>
+    </el-card>
+</template>
+
+<script>
+    import {mavonEditor} from 'mavon-editor'
+    import 'mavon-editor/dist/css/index.css'
+
+    export default {
+        name: "MarkDownTool", components: {
+            mavonEditor
+        }
+    }
+</script>
+
+<style scoped>
+    /deep/ .el-card__body {
+        padding: 0;
+    }
+</style>
