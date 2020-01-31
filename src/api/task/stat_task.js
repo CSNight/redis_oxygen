ec35223b8c5c416ea77042d1c458ec59239918b4
CSNight <923739637@qq.com>
2020-01-31 21:40:43 +0800

    add Redis statistic job manager rest api

---
+++ src/api/task/stat_task.js
@@ -0,0 +1,25 @@
+import REQ from '../../api/services'
+
+export function getStJobAll() {
+    return REQ.get('/statistic/statAll');
+}
+
+export function getStJobByUser() {
+    return REQ.get('/statistic/statByUser');
+}
+
+export function addStJob(dto) {
+    return REQ.post('/statistic/statAdd', dto);
+}
+
+export function modifyStJobConf(dto) {
+    return REQ.put('/statistic/statConfUpdate', dto);
+}
+
+export function modifyStJobState(ins, state) {
+    return REQ.put('/statistic/statStateUpdate/' + ins + "/" + state);
+}
+
+export function delStJob(ins) {
+    return REQ.delete('/statistic/statDelete/' + ins);
+}
