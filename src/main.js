22d23bb3631e001a91616de8ed77ae31452ae5d2
CSNight <923739637@qq.com>
2020-02-01 22:18:24 +0800

    add cron expression component

--- src/main.js
+++ src/main.js
@@ -5,21 +5,22 @@ import "./assets/nucleo/css/nucleo.css";
 import "./assets/fontawesome/css/fontawesome.css";
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN'
 import router from "./router/router";
 import store from "./store";
 import '@/styles/index.scss'
 import '@/icons'
 import axios from 'axios'
 import websocket from "./api/websocket";
+import i18n from './utils/lang' // Internationalization
 
 Vue.prototype.$axios = axios;
 Vue.prototype.$wss = websocket;
 axios.defaults.withCredentials = true;
 Vue.config.productionTip = false;
-Vue.use(ElementUI, {locale});
+Vue.use(ElementUI, {i18n: (key, value) => i18n.t(key, value)});
 new Vue({
     store,
     router,
+    i18n,
     render: h => h(App),
 }).$mount('#app');
---
+++ src/utils/lang.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
+import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN' // element-ui lang
+import cronEnLocale from 'vue-cron-generator/src/locale/en'
+import cronZhLocale from 'vue-cron-generator/src/locale/zh-CN'
+
+Vue.use(VueI18n);
+
+const messages = {
+    en: {
+        ...elementEnLocale,
+        ...cronEnLocale
+    },
+    zh: {
+        ...elementZhLocale,
+        ...cronZhLocale
+    }
+};
+
+const i18n = new VueI18n({
+    // set locale
+    // options: en | zh
+    locale: 'zh',
+    // set locale messages
+    messages
+});
+
+export default i18n
