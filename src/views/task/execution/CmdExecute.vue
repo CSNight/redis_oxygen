f9e1ef875ac7824d2675fad35c08179bb0f5ebed
csnight <923739637@qq.com>
2020-02-22 20:35:06 +0800

    add task menu components

---
+++ src/views/task/execution/CmdExecute.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="head-container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CmdExecute"
+    }
+</script>
+
+<style scoped>
+
+</style>
