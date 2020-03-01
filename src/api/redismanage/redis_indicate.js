be2a79c4998487e1c8aaa6b36a78c24e61720aed
csnight <923739637@qq.com>
2020-03-01 21:32:19 +0800

    fix redis indicator api url

--- src/api/redismanage/redis_indicate.js
+++ src/api/redismanage/redis_indicate.js
@@ -1,17 +1,17 @@
 import REQ from '../../api/services'
 
 export function getIndicators() {
-    return REQ.get('/indicator');
+    return REQ.get('/quota/indicator');
 }
 
 export function addIndicator(dto) {
-    return REQ.post('/indicator', dto);
+    return REQ.post('/quota/indicator', dto);
 }
 
 export function updateIndicator(dto) {
-    return REQ.put('/indicator', dto);
+    return REQ.put('/quota/indicator', dto);
 }
 
 export function deleteIndicator(id) {
-    return REQ.delete('/indicator/' + id);
+    return REQ.delete('/quota/indicator/' + id);
 }
