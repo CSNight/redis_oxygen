3f8b4377ae925bc84e880e2a9258f403333ae574
csnight <923739637@qq.com>
2020-03-01 21:33:10 +0800

    add indicator add/delete/modify function

--- src/views/rms/monitor/Indicator.vue
+++ src/views/rms/monitor/Indicator.vue
@@ -1,24 +1,31 @@
 <template>
     <div>
+        <quota-form ref="form"/>
         <div class="head-container">
-            <el-button type="primary" icon="el-icon-plus" size="mini" class="filter-item">新增
+            <el-button v-if="rights('INDICATOR_ADD')" type="primary" icon="el-icon-plus" size="mini" class="filter-item"
+                       @click="addQuota">新增
             </el-button>
-            <el-button type="danger" icon="el-icon-refresh" size="mini"/>
+            <el-button v-if="rights('INDICATOR_QUERY')" type="danger" icon="el-icon-refresh" size="mini"
+                       @click="loadIndicators"/>
         </div>
         <el-scrollbar style="height: 100%">
             <el-table style="width:100%;margin-bottom:20px;" :data="indicators" v-loading="loading" ref="indicatorList">
                 <el-table-column align="center" prop="label" label="指标名称"/>
                 <el-table-column align="center" prop="name" label="指标值"/>
-                <el-table-column align="center" prop="sign_support" label="支持信号量">
-
-                </el-table-column>
-                <el-table-column align="center" prop="exp_support" label="支持表达式">
-
-                </el-table-column>
+                <el-table-column align="center" prop="sign_support" label="支持信号量"/>
+                <el-table-column align="center" prop="exp_support" label="支持表达式"/>
                 <el-table-column align="center" prop="unit" label="指标单位"/>
                 <el-table-column align="center" prop="create_time" label="创建时间">
                     <template slot-scope="scope">
                         {{dateFormat("YYYY-mm-dd HH:MM:SS",new Date(scope.row.create_time))}}
+                    </template>
+                </el-table-column>
+                <el-table-column align="center" prop="create_time" label="操作">
+                    <template slot-scope="scope">
+                        <el-button v-if="rights('INDICATOR_UPDATE')" type="primary" icon="el-icon-edit" size="mini"
+                                   @click="updateQuota(scope.row)"/>
+                        <el-button v-if="rights('INDICATOR_DELETE')" type="danger" icon="el-icon-delete" size="mini"
+                                   @click="deleteQuota(scope.row)"/>
                     </template>
                 </el-table-column>
             </el-table>
@@ -28,13 +35,15 @@
 
 <script>
     import {dateFormat} from "@/utils/utils";
-    import {getIndicators} from "@/api/redismanage/redis_indicate";
+    import {deleteIndicator, getIndicators} from "@/api/redismanage/redis_indicate";
+    import QuotaForm from "@/views/rms/monitor/QuotaForm";
 
     export default {
         name: "Indicator",
+        components: {QuotaForm},
         data() {
             return {
-                indicators: []
+                indicators: [], loading: false
             }
         }, created() {
             this.$nextTick(() => {
@@ -58,15 +67,18 @@
                     });
                     return;
                 }
+                this.loading = true;
                 getIndicators().then((resp) => {
-                    if (resp.data.code === 200 && resp.data.status === "OK") {
+                    if (resp.data.status === 200 && resp.data.code === "OK") {
                         this.indicators = resp.data.message;
                     } else {
                         this.$message.error({
                             message: "查询出错!" + resp.data.message
                         });
                     }
+                    this.loading = false;
                 }).catch((resp) => {
+                    this.loading = false;
                     if (resp.hasOwnProperty("data")) {
                         this.$message.error({
                             message: "查询出错!" + resp.data.message
@@ -77,6 +89,45 @@
                         });
                     }
                 })
+            }, addQuota() {
+                this.$refs.form.dialog = true;
+                this.$refs.form.isAdd = true;
+            }, updateQuota(row) {
+                this.$refs.form.form = {
+                    id: row.id,
+                    name: row.name,
+                    label: row.label,
+                    exp: row.exp_support.split(";"),
+                    signal: row.sign_support.split(";"),
+                    unit: row.unit
+                };
+                this.$refs.form.dialog = true;
+                this.$refs.form.isAdd = false;
+            }, deleteQuota(row) {
+                this.$confirm('将删除该监控指标是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    deleteIndicator(row.id).then((resp) => {
+                        if (resp.data.status === 200 && resp.data.message === "success") {
+                            this.$message({
+                                type: 'success',
+                                message: '删除成功!'
+                            });
+                        } else {
+                            this.$message.error('删除失败!');
+                        }
+                        this.loadIndicators();
+                    }).catch(() => {
+                        this.$message.error('删除失败!');
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
             }
         }
     }
