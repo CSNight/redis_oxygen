52d6f9f617177260f65ce8b14748476dbb251a7b
csnight <923739637@qq.com>
2020-03-02 23:25:36 +0800

    add rule table display and delete

---
+++ src/views/task/reporter/RuleTable.vue
@@ -0,0 +1,126 @@
+<template>
+    <el-table :data="rules">
+        <el-table-column align="center" label="规则名称" prop="name"/>
+        <el-table-column align="center" label="规则描述" prop="description"/>
+        <el-table-column align="center" label="监控指标" width="100px" prop="indicator"/>
+        <el-table-column align="center" label="表达式" prop="expression"/>
+        <el-table-column align="center" label="通知级别" width="80px" prop="clazz"/>
+        <el-table-column align="center" label="联系人" prop="contact"/>
+        <el-table-column align="center" label="操作">
+            <template slot-scope="scope">
+                <el-button v-if="rights('RULES_DEL')" size="mini" icon="el-icon-delete" type="danger"
+                           @click="deleteRule(scope.row)"/>
+            </template>
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    import {deleteRule, getMonitorRuleAll, getMonitorRuleByUser} from "@/api/task/monitor_task";
+
+    export default {
+        name: "RuleTable",
+        data() {
+            return {
+                rules: [],
+            }
+        }, created() {
+            this.$nextTick(() => {
+                this.loadRules();
+            })
+        }, methods: {
+            rights(permit) {
+                if (this.$store.getters.permit.hasOwnProperty(permit)) {
+                    return this.$store.getters.permit[permit];
+                }
+                return false
+            }, loadRules() {
+                if (!this.rights("RULES_QUERY")) {
+                    this.$message.error({
+                        message: "禁止查询!"
+                    });
+                } else if (this.rights("RULES_QUERY_ALL")) {
+                    this.rules = [];
+                    this.loadAll();
+                } else {
+                    this.loadByUser();
+                }
+            }, loadAll() {
+                this.loading = true;
+                getMonitorRuleAll().then((resp) => {
+                    if (resp.data.status === 200 && resp.data.code === "OK") {
+                        this.rules = resp.data.message;
+                    } else {
+                        this.$message.error({
+                            message: "查询出错!" + resp.data.message
+                        })
+                    }
+                    this.loading = false;
+                }).catch((resp) => {
+                    if (resp.hasOwnProperty("data")) {
+                        this.$message.error({
+                            message: "查询出错!" + resp.data.message
+                        });
+                    } else {
+                        this.$message.error({
+                            message: "查询出错!" + resp.message
+                        });
+                    }
+                    this.loading = false;
+                });
+            }, loadByUser() {
+                this.loading = true;
+                getMonitorRuleByUser().then((resp) => {
+                    if (resp.data.status === 200 && resp.data.code === "OK") {
+                        this.rules = resp.data.message;
+                    } else {
+                        this.$message.error({
+                            message: "查询出错!" + resp.data.message
+                        });
+                    }
+                    this.loading = false;
+                }).catch((resp) => {
+                    if (resp.hasOwnProperty("data")) {
+                        this.$message.error({
+                            message: "查询出错!" + resp.data.message
+                        });
+                    } else {
+                        this.$message.error({
+                            message: "查询出错!" + resp.message
+                        });
+                    }
+                    this.loading = false;
+                });
+            }, deleteRule(row) {
+                this.$confirm('将删除该监控规则是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    deleteRule(row.id).then((resp) => {
+                        if (resp.data.status === 200 && resp.data.message === "success") {
+                            this.$message({
+                                type: 'success',
+                                message: '删除成功!'
+                            });
+                        } else {
+                            this.$message.error('删除失败!');
+                        }
+                        this.loadRules();
+                    }).catch(() => {
+                        this.$message.error('删除失败!');
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
